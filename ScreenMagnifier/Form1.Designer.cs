03f87c2b8247dd5b32b87d4eef359a3104c5cb16
vahid zahani <vahid.zahani@gmail.com>
2023-09-27 22:08:12 +0330

    first commit

---
+++ ScreenMagnifier/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ScreenMagnifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(452, 393);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 412);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "ScreenMagnifier";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenMagnifier/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+
+namespace ScreenMagnifier
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private Bitmap zoomedCapture;
+        private Point lastCursorPosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+            // Initialize the timer
+            timer = new Timer();
+            timer.Interval = 100; // Adjust the interval as needed
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Capture the screen around the mouse cursor
+            Point mousePosition = Cursor.Position;
+            Rectangle captureRect = new Rectangle(
+                mousePosition.X - (pictureBox.Width / 2),
+                mousePosition.Y - (pictureBox.Height / 2),
+                pictureBox.Width,
+                pictureBox.Height);
+
+            // Only capture if the cursor position has changed
+            if (mousePosition != lastCursorPosition)
+            {
+                Bitmap screenCapture = CaptureScreen(captureRect);
+
+                // Zoom the captured screen image
+                if (screenCapture != null)
+                {
+                    zoomedCapture?.Dispose();
+                    zoomedCapture = ZoomImage(screenCapture, 3); // 3X zoom
+
+                    // Update the PictureBox
+                    pictureBox.Image?.Dispose();
+                    pictureBox.Image = zoomedCapture;
+                }
+            }
+
+            lastCursorPosition = mousePosition;
+        }
+
+        private Bitmap CaptureScreen(Rectangle captureRect)
+        {
+            try
+            {
+                Bitmap screenshot = new Bitmap(captureRect.Width, captureRect.Height);
+
+                using (Graphics g = Graphics.FromImage(screenshot))
+                {
+                    g.CopyFromScreen(captureRect.Location, Point.Empty, captureRect.Size);
+                }
+
+                return screenshot;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error capturing the screen: " + ex.Message);
+                return null;
+            }
+        }
+
+        private Bitmap ZoomImage(Bitmap image, int zoomFactor)
+        {
+            int newWidth = image.Width * zoomFactor;
+            int newHeight = image.Height * zoomFactor;
+
+            Bitmap zoomedImage = new Bitmap(newWidth, newHeight);
+
+            using (Graphics g = Graphics.FromImage(zoomedImage))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(image, new Rectangle(0, 0, newWidth, newHeight));
+            }
+
+            return zoomedImage;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
